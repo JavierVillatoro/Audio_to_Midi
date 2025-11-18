7b504345097df8a8f87db8d8c7413199a5547ad1
Javier Villatoro <javierjvillatoro@gmail.com>
2025-11-19 00:38:02 +0100

    Add comment Midi size fixed

--- scripts/midi_to_labels.py
+++ scripts/midi_to_labels.py
@@ -13,10 +13,14 @@ files = [f for f in os.listdir(DATASET) if f.endswith(".wav")]
 for audio_name in files:
     midi_name = audio_name.replace("voice_", "midi_").replace(".wav", ".mid")
 
-    y, sr = librosa.load(os.path.join(DATASET, audio_name), sr=SR)
-    dur = librosa.get_duration(y=y, sr=sr)
+    #Para caso en el que no haya fijado el tamaño (ver midi_utils)
+    #y, sr = librosa.load(os.path.join(DATASET, audio_name), sr=SR)
+    #dur = librosa.get_duration(y=y, sr=sr)
 
-    labels = midi_to_sequence_window(os.path.join(DATASET, midi_name), dur)
+    labels = midi_to_sequence_window(os.path.join(DATASET, midi_name))
+    
+    # Para el caso en el que no haya fijado el tamaño 
+    #labels = midi_to_sequence_window(os.path.join(DATASET, midi_name), dur) 
     np.save(os.path.join(OUTPUT, audio_name.replace(".wav", ".npy")), labels)
 
     print("Etiquetas guardadas:", audio_name)
